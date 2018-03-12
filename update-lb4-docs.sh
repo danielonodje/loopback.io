6eab6e830baaa17c844e2e5cc7141eda48ed1cf4
biniam <biniam@ca.ibm.com>
2018-03-12 12:18:05 -0400

    use jekyll plugin for relative links

--- update-lb4-docs.sh
+++ update-lb4-docs.sh
@@ -4,19 +4,14 @@ org=strongloop
 repo=loopback-next
 target=pages/en/lb4
 branch=move-docs-into-monorepo
-tmp=$target/tempdir
+archive_name="loopback-next-$branch"
 
-mkdir -p $tmp
+mkdir -p $target
 # pull down all the markdown files (reserving dir structure)
 # given the GitHub org, repo, and branch
-curl -L https://github.com/$org/$repo/archive/$branch.tar.gz \
-   | tar --strip-components 1 -C $tmp -xzf- '*.md'
+curl -L  https://github.com/$org/$repo/archive/$target.tar.gz | tar --strip-components 1 -C "$DOCS" --wildcards \
+-xzvf- "$archive_name/docs/*.md" "$archive_name/packages/*.md" "$archive_name/docs/*.png"
 
-cp -R $tmp/packages $target/readmes/$repo
-#rename relative link extensions from md to html
-sed -E -i '' 's/\(([^(]+)\.md/(\1.html/g' $tmp/docs/*.md
+cp -R $target/packages $target/readmes/$repo
 #the following has only one occurence, but can be generalized if there is more
-sed -E -i '' 's/include previous.md/include previous.html/g' $tmp/docs/*.md
-
-mv $tmp/docs/*.md $target
-rm -rf $tmp
+sed -E -i 's/include previous.md/include previous.html/g' $target/docs/*.md
