20a8c49fd9b733fd3db7918eaab5835da1a5be35
biniam <biniam@ca.ibm.com>
2018-03-14 10:05:48 -0400

    use @loopback/docs instead

--- update-lb4-docs.sh
+++ update-lb4-docs.sh
@@ -9,8 +9,12 @@ ARCHIVE_NAME="loopback-next-$BRANCH"
 mkdir -p $DOCS
 # pull down all the markdown files (reserving dir structure)
 # given the GitHub ORG, REPO, and BRANCH
-curl -L https://github.com/$ORG/$REPO/archive/$BRANCH.tar.gz | tar --strip-components 1 -C "$DOCS" --wildcards \
--xzvf- "$ARCHIVE_NAME/docs/*.md" "$ARCHIVE_NAME/packages/*.md" "$ARCHIVE_NAME/docs/*.png"
+#curl -L https://github.com/$ORG/$REPO/archive/$BRANCH.tar.gz | tar --strip-components 1 -C "$DOCS" --wildcards \
+# -xzvf- "$ARCHIVE_NAME/docs/*.md" "$ARCHIVE_NAME/packages/*.md"
+# "$ARCHIVE_NAME/docs/*.png"
+npm i @loopback/docs --prefix $DOCS
+mv $DOCS/node_modules/@loopback/docs $DOCS/monorepo-docs
+rm -rf $DOCS/node_modules
 
 #the following has only one occurence, but can be generalized if there is more
-sed -E -i 's/include previous.md/include previous.html/g' $DOCS/docs/*.md
+#sed -E -i 's/include previous.md/include previous.html/g' $DOCS/docs/*.md
