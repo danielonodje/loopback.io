28cc58a6482fc807a80591bca836df2c87cc3acc
biniam <biniam@ca.ibm.com>
2018-03-16 14:36:53 -0400

    fixup! apply feedback

--- update-lb4-docs.js
+++ update-lb4-docs.js
@@ -2,28 +2,26 @@ const fs = require('fs-extra');
 const path = require('path');
 
 const srcDocs = path.resolve(__dirname,'node_modules/@loopback/docs/site');
-const srcImages = path.resolve(srcDocs, 'imgs');
 const destDocs = path.resolve(__dirname, 'pages/en/lb4');
-const destImages = path.resolve(__dirname, 'images/lb4');
+
+
+try {
+  fs.removeSync(destDocs);
+} catch (err) {
+  console.error('failed to cleanup previous docs folder %s', err.stack);
+  process.exit(1);
+}
 
 function copyDocs(src, dest) {
   try {
     fs.copySync(src, dest);
   } catch (err) {
-    console.error('failed to copy docs %s', err.stack);
+    console.error('failed to copy latest docs %s', err.stack);
     process.exit(1);
   }
 }
 
 copyDocs(srcDocs, destDocs);
-copyDocs(srcImages, destImages);
-try {
-  fs.removeSync(path.resolve(destDocs, 'imgs'));
-} catch (err) {
-  console.error('failed to remove duplicate imgs folder %s', err.stack);
-  process.exit(1);
-}
-
 
 const fileToUpdate = path.resolve(destDocs, 'Testing-the-API.md');
 
