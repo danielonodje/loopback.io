4853a56e66dce30384da3094032583ff19b3d851
biniam <biniam@ca.ibm.com>
2018-03-21 10:59:21 -0400

    fixup! move sidebar and remove tables

--- update-lb4-docs.js
+++ update-lb4-docs.js
@@ -3,27 +3,42 @@ const path = require('path');
 
 const srcDocs = path.resolve(__dirname,'node_modules/@loopback/docs/site');
 const destDocs = path.resolve(__dirname, 'pages/en/lb4');
-
-// Remove existing LoopBack 4 documentation content
-try {
-  fs.removeSync(destDocs);
-} catch (err) {
-  console.error('failed to cleanup previous docs folder %s', err.stack);
-  process.exit(1);
-}
+const srcSidebars = path.resolve(srcDocs, 'sidebars');
+const destSidebars= path.resolve(__dirname, '_data/sidebars');
 
 
-function copyDocs(src, dest) {
+function removeDir(dir) {
   try {
-    fs.copySync(src, dest);
+    fs.removeSync(dir);
   } catch (err) {
-    console.error('failed to copy latest docs %s', err.stack);
+    console.error('failed to cleanup %s due to %s',
+      dir, err.stack);
     process.exit(1);
   }
 }
 
+function copyDocs(src, dest) {
+  try {
+    fs.copySync(src, dest, {overwrite: true});
+  } catch (err) {
+    console.error('failed to copy latest docs %s from %s', err.stack, src);
+    process.exit(1);
+  }
+}
+
+// Remove existing LoopBack 4 documentation content
+removeDir(destDocs)
+
 // copy the latest docs from @loopback/docs to pages/en/lb4 directory
 copyDocs(srcDocs, destDocs);
+
+//copy over sidebar for LoopBack 4
+copyDocs(srcSidebars, destSidebars);
+
+//clean up sidebar dir
+removeDir(srcSidebars);
+
+
 
 const fileToUpdate = path.resolve(destDocs, 'Testing-the-API.md');
 
